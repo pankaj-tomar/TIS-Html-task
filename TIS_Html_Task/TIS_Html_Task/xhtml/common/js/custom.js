30c3750702b76e5126020ae352a5f97c0842b9f0
Pankaj Kumar <pankaj1.k@techmatrixconsulting.com>
2022-09-02 16:16:35 +0530

    changes

---
+++ TIS_Html_Task/TIS_Html_Task/xhtml/common/js/custom.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+  $( ".flex-control-nav, .flex-direction-nav" ).wrap( "<div class='cont-wrap'></div>" );
+  $( ".cont-wrap" ).wrap( "<div class='wrapper-cont'></div>" );
+  
+});
+
+
+
+
+
+
+  
